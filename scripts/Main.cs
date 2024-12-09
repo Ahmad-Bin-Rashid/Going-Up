4b79371e2d9e32cec443704ff322348f58d42fc6
Ahmad Bin Rashid <ahmadbinrashid92@gmail.com>
2024-12-09 15:03:32 +0500

    eww, spelling mistake

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -11,7 +11,7 @@ namespace GoingDown
 		public override void _Ready()
 		{
 
-			// This gets a reference to command Processor (Do not change it my childern)
+			// This gets a reference to command Processor (Do not change it my children)
 			
 			//Moved stuff to Global.cs in scripts/GlobalScripts/Global.cs
 			InputManager.Instance.EnterRoom(Global.Instance.mapManager.GetCurrentRoom());
