5991b5d053652ec60693e98117ffc66479ebcd94
omer ghazali <77153164+MuhammadOmerGhazali@users.noreply.github.com>
2024-12-15 10:39:51 +0000

    added screen buffer in utilis

--- Going Up/Utility.h
+++ Going Up/Utility.h
@@ -1,13 +1,9 @@
 #pragma once
 
-#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
 #include "Room.h"
-
-void SetCursorAtXY(int x, int y) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD pos = {static_cast<SHORT>(x), static_cast<SHORT>(y)};
-    SetConsoleCursorPosition(hConsole, pos);
-}
 
 std::string roomTypeToString(RoomType roomType) {
     switch (roomType) {
@@ -28,3 +24,45 @@ RoomType stringToRoomType(const std::string& type) {
     if (type == "Treasure") return RoomType::Treasure;
     return RoomType::Spawn;
 }
+class ScreenBuffer
+{
+    private:
+        std::vector<std::string> buffer;
+        int width, height;
+    
+    public:
+        ScreenBuffer(int w, int h) : width(w), height(h), buffer(h, std::string(w, ' ')) {}
+
+        void setChar(int x, int y, char c) 
+        {
+            if (y >= 0 && y < height && x >= 0 && x < width) 
+                {
+                    buffer[y][x] = c;
+                }
+        }
+
+        void setString(int x, int y, const std::string &str) 
+        {
+            if (y >= 0 && y < height) 
+            {
+                for (size_t i = 0; i < str.size() && x + i < width; ++i) 
+                {
+                    buffer[y][x + i] = str[i];
+                }
+            }
+        }
+        void clear() 
+        {
+            for (auto &row : buffer) 
+                {
+                    row = std::string(width, ' ');
+                }
+        }
+        void print() const 
+        {
+            for (const auto &row : buffer) 
+                {
+                    std::cout << row << '\n';
+                }
+        }
+};
